09550af793475d3c812db283ff49e4e18ea50bbb
Caio Braga <caiotabraga@hotmail.com>
2022-07-28 02:12:08 -0300

    Project created

---
+++ console-chess/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace console_chess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
